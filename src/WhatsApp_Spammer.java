fa9642e8b3683aadf23dbcbd70847051a564e35a
Rupak Roy <63533688+rupakroy12@users.noreply.github.com>
2021-08-29 23:29:30 +0530

    New commit

---
+++ src/WhatsApp_Spammer.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WhatsApp_Spammer {
+
+	public static void main(String args[]) throws Exception {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(150, TimeUnit.SECONDS);
+
+		driver.get("https://web.whatsapp.com/");
+
+		String message;
+		String confirm;
+		int number;
+
+		Scanner sc = new Scanner(System.in);
+
+		while (true) {
+
+			try {
+				System.out.println("Enter Message : ");
+				message = sc.nextLine();
+
+				System.out.println("Enter number of messages : ");
+				number = sc.nextInt();
+
+				System.out.println("Are You sure, You want to spam  "
+						+ driver.findElement(By.xpath("//*[@id=\"main\"]/header/div[2]/div/div/span")).getText()
+						+ " ?");
+				System.out.println("Type Y to continue : ");
+
+				sc.nextLine();
+				confirm = sc.nextLine();
+
+				if (confirm.equalsIgnoreCase("Y")) {
+					for (int i = 1; i <= number; i++) {
+						try {
+							driver.findElement(By.xpath("//*[@id=\"main\"]/footer/div[1]/div[2]/div/div[1]/div/div[2]"))
+									.sendKeys(message + Keys.ENTER);
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+
+					}
+					System.out.println(number + " messages sent successfully to "
+							+ driver.findElement(By.xpath("//*[@id=\"main\"]/header/div[2]/div/div/span")).getText());
+					System.out.println("___________________________________________");
+
+				} else {
+					System.out.println("Sorry, you did not type Y");
+					System.out.println("___________________________________________");
+				}
+			} catch (Exception e) {
+				sc.nextLine();
+				System.out.println("___________________________________________");
+
+			}
+
+		}
+
+	}
+}
